a65ca0765d8ecb25b6f6666a35508bfccfd6fad6
Mitchell Long <meechllada@gmail.com>
2017-09-15 09:40:28 -0700

    set up routes to allow word to be printed to the screen

--- app.rb
+++ app.rb
@@ -3,3 +3,17 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word_definer')
 require('pry')
+
+get('/') do
+  @list = Word.all()
+  erb(:list)
+end
+
+post('/') do
+  word = params['word']
+  definition = params['definition']
+  vocab_word = Word.new(word, definition)
+  vocab_word.save()
+  @list = Word.all()
+  erb(:list)
+end
