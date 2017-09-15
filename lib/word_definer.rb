8f28b8f0f74f363482912fcc57083ba597afe248
Mitchell Long <meechllada@gmail.com>
2017-09-15 09:04:05 -0700

    add #save and .clear unit tests

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,12 +1,18 @@
 class Word
- attr_accessor :word :definition
+
+  @@list = []
+ attr_accessor :word, :definition
 
  def initialize(word, definition)
    @word = word
    @definition = definition
  end
- 
+
  def self.all()
+   @@list
+ end
+ def save ()
+   @@list.push(self)
  end
 
 end
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -1,8 +1,29 @@
 require 'rspec'
 require 'word_definer'
 
-describe('') do
-  it'' do
+
+describe('.all') do
+  it 'is empty at first' do
+    expect(Word.all()).to(eq([]))
+  end
+end
+describe('#save') do
+  it('saves a word and adds it to a list of words') do
+    word = Word.new("eagle","a large bird of prey with a massive hooked bil" )
+    word.save()
+    expect(Word.all()).to(eq([word]))
+  end
+end
+describe('.clear') do
+    it('clears all items from the list of definitions') do
+    word = Word.new("eagle","a large bird of prey with a massive hooked bil" )
+    word.save()
+    Item.clear()
+    expect(Word.all()).to(eq([]))
+  end
+end
+describe('Word') do
+  it'takes a word and an array that contains all words' do
     expect().to(eq())
   end
 end
